171801355f5ddb576c1c082588f3d7737cacd6d1
Aaron <sergio.bravo.9406@gmail.com>
2025-11-06 01:15:04 -0300

    TP Angular completo - Gestión de Estudiantes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { StudentListComponent } from './components/student-list/student-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: StudentListComponent },
+  { path: '**', redirectTo: '' }
+];
